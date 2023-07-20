fd346bfa5313d388f850f303936e30a33ffbbf5c
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-07-20 18:06:12 +0300

    Added models for airport app

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import (
+    Airport,
+    Route,
+    AirplaneType,
+    Airplane,
+    Crew,
+    Flight,
+    Order,
+    Ticket,
+)
+
+
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("name", "closest_big_city")
+    list_filter = ("name",)
+    search_fields = ("name", "closest_big_city")
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ("source", "destination", "distance")
+    list_filter = ("source", "destination",)
+    search_fields = ("source", "destination",)
+
+
+@admin.register(Airplane)
+class AirplaneAdmin(admin.ModelAdmin):
+    list_display = ("name", "airplane_type", "rows", "seats_in_row")
+    list_filter = ("name", "airplane_type",)
+    search_fields = ("name", "airplane_type",)
+
+
+@admin.register(Crew)
+class CrewAdmin(admin.ModelAdmin):
+    list_display = ("position", "first_name", "last_name")
+    list_filter = ("position",)
+    search_fields = ("position",)
+
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("airplane", "route", "departure_time", "arrival_time")
+    list_filter = ("airplane", "route", "departure_time", "arrival_time")
+    search_fields = ("airplane", "route",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("created_at",)
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("order", "flight", "row", "seat", "price")
+    list_filter = ("order", "flight", "price")
+
+
+admin.site.register(AirplaneType)
--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,143 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
+
+
+def airplane_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/airplanes/", filename)
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    closest_big_city = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="route_source"
+    )
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="route_destination"
+    )
+    distance = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.id)
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+    image = models.ImageField(
+        null=True, upload_to=airplane_image_file_path
+    )
+    airplane_type = models.ForeignKey(
+        AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
+    )
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Crew(models.Model):
+    CAPTAIN = "Captain"
+    FIRST_OFFICER = "First Officer"
+    NAVIGATOR = "Navigator"
+    FLIGHT_ENGINEER = "Flight Engineer"
+    FLIGHT_ATTENDANT = "Flight Attendant"
+    UNKNOWN = "unknown"
+
+    POSITION_CHOICES = [
+        (CAPTAIN, "Captain"),
+        (FIRST_OFFICER, "First Officer"),
+        (NAVIGATOR, "Navigator"),
+        (FLIGHT_ENGINEER, "Flight Engineer"),
+        (FLIGHT_ATTENDANT, "Flight Attendant"),
+        (UNKNOWN, "unknown"),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    position = models.CharField(
+        max_length=100, choices=POSITION_CHOICES, default=UNKNOWN
+    )
+
+    def __str__(self):
+        return f"{self.position}: {self.first_name} {self.last_name}"
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route, on_delete=models.CASCADE, related_name="rout_flight"
+    )
+    airplane = models.ForeignKey(
+        Airplane, on_delete=models.CASCADE, related_name="airplane_flight"
+    )
+    departure_time = models.DateTimeField(blank=True, null=True)
+    arrival_time = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0
+    )
+    flight = models.ForeignKey(
+        Flight, on_delete=models.CASCADE, related_name="flight_ticket"
+    )
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="tickets"
+    )
+
+    class Meta:
+        unique_together = ("flight", "row", "seat")
+        ordering = ("row", "seat")
+
+    def __str__(self):
+        return (
+            f"{str(self.flight)} (row: {self.row}, seat: {self.seat})"
+        )
