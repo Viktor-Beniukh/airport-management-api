91a8bdb064044fbb38a64efb2a1ab6de26573eaa
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-07-23 18:19:16 +0300

    Added tests for user app

---
+++ user/tests/test_user_model.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class ModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="test@test.com",
+            password="testpass",
+            first_name="John",
+            last_name="Doe"
+        )
+
+    def test_user_str(self) -> None:
+        expected_str = self.user.email
+
+        self.assertEqual(str(self.user), expected_str)
+
+    def test_user_full_name_property(self) -> None:
+        expected_full_name = f"{self.user.first_name} {self.user.last_name}"
+
+        self.assertEqual(self.user.full_name, expected_full_name)
