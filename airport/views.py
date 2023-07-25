0350d19c17181eb2a678aa8cd46200aa54e98252
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-07-25 14:16:50 +0300

    Optimized upload_image method of AirplaneViewSet

--- airport/views.py
+++ airport/views.py
@@ -105,11 +105,9 @@ class AirplaneViewSet(viewsets.ModelViewSet):
         airplane = self.get_object()
         serializer = self.get_serializer(airplane, data=request.data)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @extend_schema(
         parameters=[
